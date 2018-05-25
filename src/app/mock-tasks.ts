c455bb698157469d8c06f4668e998dfd0997c634
Pankushg <39335738+Pankushg@users.noreply.github.com>
2018-05-25 19:22:46 +0530

    Add files via upload

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,9 @@
+import { Task } from './task';
+
+export const TASKS: Task[] = [
+  { id: 101, name: 'Folder Checking' },
+  { id: 102, name: 'Check Archival Job' },
+  { id: 103, name: 'Monitoring Error Batches' },
+  { id: 104, name: 'URL Working/Not Working' },
+  { id: 105, name: 'Checking DB jobs' },
+];
